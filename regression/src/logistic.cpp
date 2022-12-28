10b96913d93b2f9a574ba7d1e3895f8dc2d5fccb
longwatermelon <longwatermelon1@gmail.com>
2022-12-27 23:18:51 -0700

    set up logistic

---
+++ regression/src/logistic.cpp
@@ -0,0 +1,51 @@
+#include <SDL2/SDL.h>
+#include <graph2.h>
+
+namespace logistic
+{
+}
+
+int main(int argc, char **argv)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *win = SDL_CreateWindow("Logistic regression",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            600, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer *rend = SDL_CreateRenderer(win, -1,
+            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    bool running = true;
+    SDL_Event evt;
+
+    Graph2 graph("data-logistic/graph");
+    float w = 0.f,
+          b = 0.f;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&evt))
+        {
+            switch (evt.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            }
+        }
+
+        SDL_RenderClear(rend);
+
+        graph.render(rend, { 0, 0, 600, 600 }, [w, b](float x){
+            return 1.f / (1 + std::exp(-(w * x + b)));
+        });
+
+        SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+        SDL_RenderPresent(rend);
+    }
+
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
+
