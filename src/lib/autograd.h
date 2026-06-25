0c73fa46f76372a2c512c7bd95f36f69a23c588f
longwatermelon <kt@yami.so>
2026-06-25 14:25:25 -0700

    2026-06-25 14:25

--- src/lib/autograd.h
+++ src/lib/autograd.h
@@ -7,7 +7,6 @@ namespace autograd {
 
 enum class FnType {
     Matmul,
-    Scale,
 };
 
 struct Value {
@@ -18,14 +17,12 @@ struct Value {
     // compute cached result, assuming adj results are all populated
     void compute_result();
 
-    // compute grad from parent value v using chain rule
-    void compute_grad(Value *v);
+    // add chain rule contrib to grads of children in adj
+    void add_grads();
 };
 
 // matmul AB
 shared_ptr<Value> calc_matmul(shared_ptr<Value> A, shared_ptr<Value> B);
-// scaling matrix A
-shared_ptr<Value> calc_scale(shared_ptr<Value> c, shared_ptr<Value> A);
 
 // traverse DAG topologically and compute grads
 void compute_grads(shared_ptr<Value> root);
