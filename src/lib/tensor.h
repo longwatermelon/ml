47beae7e3433cc72d432215dd99f975c89618c1b
longwatermelon <kt@yami.so>
2026-06-26 15:19:04 -0700

    2026-06-26 15:19

--- src/lib/tensor.h
+++ src/lib/tensor.h
@@ -17,30 +17,40 @@ struct Tensor {
     vec<int> shape, stride;
     vec<double> data;
 
+    // ---- ctors ----
+
     Tensor() = default;
 
     Tensor(const vec<double> &data_1d);
     Tensor(const vec2<double> &data_2d);
     Tensor(const vec<int> &shape, double value);
 
-    // shape ops
+    // ---- shape ops ----
+
+    // reinterpret same data with different shape
     void reshape(const vec<int> &new_shape);
+    // broadcast shape to match new_shape
     void broadcast(const vec<int> &new_shape);
     // sum-reduce along all axes i where shape[i]=1
     void unbroadcast(const vec<int> &shape);
-    // p[i]: shape[i] := shape[p[i]]
+    // re-order axis arg order while maintaining semantic meaning of axes
     void permute(const vec<int> &p);
+    // left-pad axes of current shape to match target shape's dimension cnt
     void pad_shape(const vec<int> &target);
     // consolidate data, become contiguous again
     Tensor materialize() const;
     // return if tensor is contiguous
     bool is_contiguous() const;
 
-    // element access
+    // ---- element access ----
+
+    // lvalue ref
     double &at(const vec<int> &ind);
+    // rvalue
     double at(const vec<int> &ind) const;
 
-    // element-wise arithmetic with another tensor
+    // ---- arithmetic / operations ----
+
     Tensor operator+(const Tensor &o) const;
     Tensor operator-(const Tensor &o) const;
     Tensor &operator+=(const Tensor &o);
@@ -49,23 +59,27 @@ struct Tensor {
     Tensor hadamard(const Tensor &o) const;
     // element-wise div
     Tensor ediv(const Tensor &o) const;
+    // unary negation
     Tensor operator-() const;
-
-    // matrix ops --- operates on last two dims, parallelized across all previous dims
+    // matmul on least significant two axes, parallelized across the rest
     Tensor operator*(const Tensor &o) const;
+    // transpose on least significant two axes, parallelized across the rest
     Tensor transpose() const;
+    // sum-reduce along an axis. keepdims = if reduced axis remains as len 1 or gets deleted.
+    Tensor sum(int axis, bool keepdims) const;
+    // return tensor of argmin indices along axis arrays
+    Tensor argmin(int axis) const;
+    // return tensor of argmax indices along axis arrays
+    Tensor argmax(int axis) const;
 
-    // element-wise function application
+    // ---- functionals ----
+
+    // apply to copy of this
     Tensor apply(const std::function<double(double)> &f) const;
-    // applies function between two tensors, auto-broadcasts up to one tensor if needed
+    // applies function between two tensors, auto-broadcasts both tensors as needed
     Tensor apply(const Tensor &o, const std::function<double(double, double)> &f) const;
     // apply to this, return ref to this
     Tensor &apply_inplace(const std::function<double(double)> &f);
-    // apply to this, return ref to this
+    // applies function between two tensors, store result in this, auto-broadcast both tensors as needed
     Tensor &apply_inplace(const Tensor &o, const std::function<double(double, double)> &f);
-
-    // reductions
-    Tensor sum(int axis, bool keepdims) const;
-    Tensor argmin(int axis) const;
-    Tensor argmax(int axis) const;
 };
